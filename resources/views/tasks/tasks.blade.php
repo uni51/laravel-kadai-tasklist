19ca250fa74a4a0f100f4bca0ea01a920721fd47
vagrant <vagrant@homestead>
2018-07-13 04:35:33 +0000

    modified

--- resources/views/tasks/tasks.blade.php
+++ resources/views/tasks/tasks.blade.php
@@ -10,7 +10,7 @@
                 {!! link_to_route('users.show', $user->name, ['id' => $user->id]) !!} <span class="text-muted">posted at {{ $task->created_at }}</span>
             </div>
             <div>
-                <p>{!! nl2br(e($task->content)) !!}</p>
+                <p>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!} : {!! e($task->status) !!} > {!! nl2br(e($task->content)) !!}</p>
             </div>
             <div>
                 @if (Auth::user()->id == $task->user_id)
